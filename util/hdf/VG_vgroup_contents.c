d05b36c77a7e092a4a72aa8b3d05f549c3548cb9
takano32 <takano32@gmail.com>
2013-04-20 23:09:02 +0900

    fix for MOD35 data.

--- util/hdf/VG_vgroup_contents.c
+++ util/hdf/VG_vgroup_contents.c
@@ -1,9 +1,12 @@
+#include <string.h>
 #include "hdf.h"
 
 #define   FILE_NAME        "General_Vgroups.hdf"
-#define   VDATA_NAME       "Temperature"
-#define   FIELDNAME_LIST   "TMP"
+#define   VDATA_NAME       "Latitude"
+#define   MAX_FIELDNAME_LIST_LENGTH (1024 * 1024)
 #define   RECORD_INDEX     0
+#define   N_RECORDS        406
+#define   N_VALS_PER_REC   270
 
 int main( )
 {
@@ -12,71 +15,52 @@ int main( )
    intn   status_n;     /* returned status for functions returning an intn  */
    int32  status_32,    /* returned status for functions returning an int32 */
           file_id, vgroup_id, vgroup_ref, vdata_id, vdata_ref,
-	  num_of_records, record_pos,
+	  num_of_records, num_of_vals_per_rec, record_pos,
           obj_index,    /* index of an object within a vgroup */
           num_of_pairs, /* number of tag/ref number pairs, i.e., objects */
           obj_tag, obj_ref,     /* tag/ref number of an HDF object */
           vgroup_pos = 0;       /* position of a vgroup in the file */
-   float32 databuf[1][1];
+   float32 databuf[N_RECORDS][N_VALS_PER_REC];
    int16 rec_num;
 
    /********************** End of variable declaration ***********************/
 
-   /*
-    * Open the HDF file for reading.
-    */
+   char FIELDNAME_LIST[MAX_FIELDNAME_LIST_LENGTH];
+   memset(FIELDNAME_LIST, 0x00, MAX_FIELDNAME_LIST_LENGTH);
+   int i;
+   for (i = 0; i < N_VALS_PER_REC; i++) {
+	   char buffer[256];
+	   if (i == 0) {
+		   sprintf(buffer, "%d", i);
+	   } else {
+		   sprintf(buffer, " %d", i);
+	   }
+	   strcat(FIELDNAME_LIST, buffer);
+   }
    file_id = Hopen (FILE_NAME, DFACC_READ, 0);
 
-   /*
-    * Initialize the V interface.
-    */
    status_n = Vstart (file_id);
 
-   /*
-    * Obtain each vgroup in the file by its reference number, get the
-    * number of objects in the vgroup, and display the information about
-    * that vgroup.
-    */
-   vgroup_ref = -1;        /* set to -1 to search from the beginning of file */
-   /*
-    * Get the reference number of the next vgroup in the file.
-    */
-   vgroup_ref = Vgetid (file_id, vgroup_ref);
+   vgroup_ref = -1;
 
-   /*
-    * Attach to the vgroup for reading or exit the loop if no more vgroups
-    * are found.
-    */
-   if (vgroup_ref == -1) break;
+   vgroup_ref = Vgetid (file_id, vgroup_ref);
+   if (vgroup_ref == -1) exit(1);
+   vgroup_ref = Vgetid (file_id, vgroup_ref);
+   if (vgroup_ref == -1) exit(1);
+
    vgroup_id = Vattach (file_id, vgroup_ref, "r"); 
 
-   /*
-    * Get the total number of objects in the vgroup.
-    */
    num_of_pairs = Vntagrefs (vgroup_id);
 
-   /*
-    * If the vgroup contains any object, print the tag/ref number 
-    * pair of each object in the vgroup, in the order they appear in the
-    * file, and indicate whether the object is a vdata, vgroup, or neither.
-    */
    if (num_of_pairs > 0)
    {
 	   printf ("\nVgroup #%d contains:\n", vgroup_pos);
 	   for (obj_index = 0; obj_index < num_of_pairs; obj_index++)
 	   {
-		   /*
-		    * Get the tag/ref number pair of the object specified 
-		    * by its index, obj_index, and display them.
-		    */
 		   status_n = Vgettagref (vgroup_id, obj_index, &obj_tag, &obj_ref);
 		   printf ("tag = %d, ref = %d", obj_tag, obj_ref);
 
 
-		   /*
-		    * State whether the HDF object referred to by obj_ref is a vdata,
-		    * a vgroup, or neither.
-		    */
 		   if (Visvg (vgroup_id, obj_ref))
 			   printf ("  <-- is a vgroup\n");
 		   else if (Visvs (vgroup_id, obj_ref)) {
@@ -86,10 +70,13 @@ int main( )
 			   status_n = VSsetfields (vdata_id, FIELDNAME_LIST);
 			   record_pos = VSseek (vdata_id, RECORD_INDEX);
 			   num_of_records =
-				   VSread (vdata_id, (uint8 *)databuf, 1, FULL_INTERLACE);
+				   VSread (vdata_id, (uint8 *)databuf, N_RECORDS, FULL_INTERLACE);
 			   for (rec_num = 0; rec_num < num_of_records; rec_num++)
 			   {
-				   printf ("%6.2f\n", databuf[rec_num][0]);
+				   for (num_of_vals_per_rec = 0; num_of_vals_per_rec < N_VALS_PER_REC; num_of_vals_per_rec++) {
+					   printf ("%6.2f\n", databuf[rec_num][num_of_vals_per_rec]);
+
+				   }
 			   }
 		   }
 		   else
