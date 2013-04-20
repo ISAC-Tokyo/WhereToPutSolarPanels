9dd5c6fb2132616cf19f061e5276d135d1083aa3
takano32 <takano32@gmail.com>
2013-04-21 00:43:27 +0900

    add print_data_size.rb

---
+++ util/hdf/print_data_size.rb
@@ -0,0 +1,15 @@
+#! /usr/bin/env ruby
+# -*- coding: utf-8 -*-
+# vim: set noet sts=4 sw=4 ts=4 fdm=marker :
+#
+# Parse Latitude
+# author: takano32 <tak@no32 dot tk>
+#
+
+
+
+if __FILE__ == $0 then
+	data = ARGF.read.split %r!\s+!
+	puts data.size
+end
+
