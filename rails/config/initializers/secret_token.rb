4fdaedaaeb137c773f8ff9daf986f8d2bce7d29d
Matsue <hiroki.matsue@gmail.com>
2013-04-20 19:55:15 +0900

    init rails directory.

---
+++ rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhereToPutSolarPanels::Application.config.secret_token = '3e06a9bf44f17783893d9a6e96afdcfbf703b4c7a3d832dddd73e07cc1b2d845612fb0e1c4cdfa3ec77df7c362b8310fbcfb5b7a5086abea606b3d98003d7d8e'
