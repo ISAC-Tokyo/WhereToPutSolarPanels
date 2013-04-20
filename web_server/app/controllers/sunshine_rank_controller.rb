d616f3e3b8c8cadd0c81fb624b83f68f821138ea
Eric Platon <zaraki@gmx.com>
2013-04-21 01:57:19 +0900

    Fix typo in the API.

--- web_server/app/controllers/sunshine_rank_controller.rb
+++ web_server/app/controllers/sunshine_rank_controller.rb
@@ -1,19 +1,19 @@
 class SunshineRankController < ApplicationController
   def get_rank
     lat = params[:lat]
-    lan = params[:lan]
+    lon = params[:lon]
 
-    ret = Cloud.find_by_geo(lat, lan)
+    ret = Cloud.find_by_geo(lat, lon)
     render :json => ret.to_json
   end
 
   def get_range_rank
     lat = params[:lat]
-    lan = params[:lan]
+    lon = params[:lon]
     scale = params[:scale]
     range = culc_range(scale)
 
-    ret = Cloud.find_range_by_geo(lat, lan, range)
+    ret = Cloud.find_range_by_geo(lat, lon, range)
     render :json => ret.to_json
   end
 
