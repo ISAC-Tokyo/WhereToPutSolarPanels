42b0046c412e0fa48921e0e4f5d9b2ee6029a669
Hiroki Matsue <hiroki.matsue@gmail.com>
2013-04-21 09:08:35 +0900

    Adjust response of range_rank for production.

--- web_server/app/controllers/sunshine_rank_controller.rb
+++ web_server/app/controllers/sunshine_rank_controller.rb
@@ -5,6 +5,7 @@ class SunshineRankController < ApplicationController
     "PROBABLY_CLEAR" => 2,
     "CONFIDENT_CLEAR" => 3
   }
+  DIV_NUM = 400
   RANGE = 1
 
   def get_rank
@@ -28,20 +29,36 @@ class SunshineRankController < ApplicationController
   end
 
   def get_range_rank
-    lat = params[:lat]
-    lon = params[:lon]
-    scale = params[:scale]
-    range = culc_range(scale)
+    lat_s = params[:lat_s]
+    lat_e = params[:lat_e]
+    lon_s = params[:lon_s]
+    lon_e = params[:lon_e]
 
-    ret = Cloud.find_range_by_geo(lat, lon, range)
+    raw = Cloud.where(:latitude.gt => lat_s, :latitude.lt => lat_e, :longitude.gt => lon_s, :longitude.lt => lon_e).to_a
+
+    ret = []
+    step = raw.length / DIV_NUM
+    DIV_NUM.times do |i|
+      idx = (step * (i-1)).to_i
+      new = {}
+      new["lat"] = raw[idx]["latitude"]
+      new["lon"] = raw[idx]["longitude"]
+      new["weight"] = SCORES[raw[idx]["quority"]]
+      ret << new
+    end
+
     render :json => ret.to_json
   end
 
   private
 
-  def culc_range(scale)
-    # dummy
-    ret = [35.713382, 139.762376]
-    ret
+  def get_summary(raw)
+    sum = 0
+    raw.each do |data|
+      date = data["date"]
+      score = SCORES[data["quority"]]
+      sum += score
+    end
+    sum
   end
 end
