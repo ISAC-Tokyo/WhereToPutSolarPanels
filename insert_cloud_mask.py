f2036968fdddf43739f31fb0183ad2b7c483f413
takano32 <takano32@gmail.com>
2013-04-22 20:29:26 +0900

    calc date from file_name.

--- insert_cloud_mask.py
+++ insert_cloud_mask.py
@@ -8,15 +8,18 @@
 import h5py
 import pymongo
 
-import datetime
-
-#file_name = 'MOD35_L2.A2004274.1140.005.2010148215256.h5'
-#> ruby -rdate -e 'p Date.new(2004, 1, 1) + 274 - 1'
-# => <Date: 2004-09-30 ((2453279j,0s,0n),+0s,2299161j)>
 
 import sys
 file_name = sys.argv[1]
 f = h5py.File(file_name, 'r')
+
+import datetime
+import re
+m = re.match('^MOD35_L2.A([0-9]{4})([0-9]{3}).*', file_name)
+year = int(m.group(1))
+yday = int(m.group(2)) - 1
+datedelta = datetime.timedelta(yday)
+date = datetime.datetime(year, 1, 1) + datedelta
 
 DEBUG = False
 
@@ -29,8 +32,9 @@ CLOUD_MASK = int('00000110', 2)
 LAND_OR_WATER = int('11000000', 2)
 WATER = int('00000000', 2)
 
-db = pymongo.Connection('10.1.1.82', 27017).test
+db = pymongo.Connection('10.1.1.82', 27017).wtps
 mongo = db.cloud_mask
+
 
 def skip(lat, lon):
     if 36.0 < lat:
@@ -38,33 +42,35 @@ def skip(lat, lon):
     if lat < 32.0:
         return True
     if 142.0 < lon:
-       return True
+        return True
     if lon < 138.0:
-       return True
-   return False
+        return True
+    return False
 
 
 z = 0
 cloud_mask = cloud_masks[z]
 for y in range(0, len(latitude)):
     for x in range(0, len(latitude[0])):
-        print "x, y, z = %s, %s, %s" % (x, y, z)
         cm = cloud_mask[y * 5][x * 5]
         lat = latitude[y][x]
         lon = longitude[y][x]
-        if cm & LAND_OR_WATER == WATER: continue
+        if cm & LAND_OR_WATER == WATER:
+            continue
         score = (cm & CLOUD_MASK) >> 1
+        print "x, y, z = %s, %s, %s" % (x, y, z)
         print "Latitude: %s" % lat
         print "Longitude: %s" % lon
         print "Score: %d" % score
-        mongo.insert({
-            'date': datetime.datetime(2012, 1, 1),
+        data = {
+            'date': date,
             'lat': float(lat),
             'lon': float(lon),
             'score': int(score),
-        })
+        }
+        mongo.insert(data)
         print "----"
+
+print date
+f.close()
 print "done"
-
-
-
