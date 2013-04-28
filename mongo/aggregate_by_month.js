d69dc116c69890ceb380d6fdc0e9f87b5e040492
hagino3000 <hagino.3000@gmail.com>
2013-04-28 23:25:09 +0900

    Round score and low

--- mongo/aggregate_by_month.js
+++ mongo/aggregate_by_month.js
@@ -2,28 +2,28 @@ var COORDINATE_DECIMAL = 2;
 
 print('Start aggregate by location and date')
 print(Date());
+
+// 小数桁を指定したまるめ
+// round(49.11627197265625, 3) => 49.116
+function round(val, decimal) {
+  val = val * Math.pow(10, decimal);
+  val = Math.round(val);
+  val = val / Math.pow(10, decimal);
+  return val;
+}
+
+// ISODate to "201301"
+function toMonth(d) {
+  var year = d.getFullYear();
+  var month = d.getMonth() + 1;
+  if (month < 10) {
+    month = '0' + month;
+  }
+  return String(year) + String(month);
+}
  
 // lat, lonが近い物が同じキーとなる
 function map() {
-
-  // 位置のまるめ
-  // round(49.11627197265625, 3) => 49.116
-  var round = function(val, decimal) {
-    val = val * Math.pow(10, decimal);
-    val = Math.round(val);
-    val = val / Math.pow(10, decimal);
-    return val;
-  }
-  // ISODate to "201301"
-  var toMonth = function(d) {
-    var year = d.getFullYear();
-    var month = d.getMonth() + 1;
-    if (month < 10) {
-      month = '0' + month;
-    }
-    return String(year) + String(month);
-  }
-  
   var key = toMonth(this.date) + '_' + round(this.loc.lat, COORDINATE_DECIMAL) + '_' + round(this.loc.lon, COORDINATE_DECIMAL);
   emit(key, {
     score: this.score,
@@ -62,8 +62,8 @@ function finalize(key, value) {
     totalScore: value.score,
     totalLow: value.low,
     count: value.count,
-    score: value.score / value.count,
-    low: value.low / value.count
+    score: round(value.score / value.count, 4),
+    low: round(value.low / value.count, 4)
   }
 }
 
@@ -72,6 +72,8 @@ var res = db.cloud_mask.mapReduce(map, reduce, {
   out: outCollection,
   finalize: finalize,
   scope: {
+    round: round,
+    toMonth: toMonth,
     COORDINATE_DECIMAL: COORDINATE_DECIMAL
   },
   verbose: true
--- mongo/aggregate_date.js
+++ mongo/aggregate_date.js
@@ -3,18 +3,18 @@ var COORDINATE_DECIMAL = 1;
 
 print('Start aggregate by location and date')
 print(Date());
+
+// 小数桁を指定したまるめ
+// round(49.11627197265625, 3) => 49.116
+function round(val, decimal) {
+  val = val * Math.pow(10, decimal);
+  val = Math.round(val);
+  val = val / Math.pow(10, decimal);
+  return val;
+}
  
 // lat, lonが近い物が同じキーとなる
 function map() {
-
-  // 位置のまるめ
-  // round(49.11627197265625, 3) => 49.116
-  var round = function(val, decimal) {
-    val = val * Math.pow(10, decimal);
-    val = Math.round(val);
-    val = val / Math.pow(10, decimal);
-    return val;
-  }
   var key = round(this.loc.lat, COORDINATE_DECIMAL) + '_' + round(this.loc.lon, COORDINATE_DECIMAL);
   emit(key, {
     score: this.score,
@@ -52,8 +52,8 @@ function finalize(key, value) {
     totalScore: value.score,
     totalLow: value.low,
     count: value.count,
-    score: value.score / value.count,
-    low: value.low / value.count
+    score: round(value.score / value.count, 4),
+    low: round(value.low / value.count, 4)
   }
 }
 
@@ -62,6 +62,7 @@ var res = db.cloud_mask.mapReduce(map, reduce, {
   out: outCollection,
   finalize: finalize,
   scope: {
+    round: round,
     COORDINATE_DECIMAL: COORDINATE_DECIMAL
   },
   verbose: true
