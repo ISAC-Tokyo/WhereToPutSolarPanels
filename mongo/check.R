b82f563d5f2817aa66d607281086c3d0d649f57c
hagino3000 <hagino.3000@gmail.com>
2013-04-30 00:56:36 +0900

    Check distribution of score average

--- mongo/check.R
+++ mongo/check.R
@@ -1,2 +1,7 @@
-ranks <- read.delim('./alldate_scale1.csv', sep = ',', header = TRUE)
-summary(ranks)
+values <- read.delim('./alldate_scale1.csv', sep = ',', header = TRUE)
+summary(values)
+
+head(scores)
+
+scores = with(values, value.score)
+hist(scores)
