1da309371543f66dc404b0b7ff5e4c1946552b38
hagino3000 <hagino.3000@gmail.com>
2013-05-02 03:03:04 +0900

    Comment out unused tasks

--- web_client/Gruntfile.js
+++ web_client/Gruntfile.js
@@ -24,18 +24,18 @@ module.exports = function (grunt) {
     grunt.initConfig({
         yeoman: yeomanConfig,
         watch: {
-            coffee: {
-                files: ['<%= yeoman.app %>/scripts/{,*/}*.coffee'],
-                tasks: ['coffee:dist']
-            },
-            coffeeTest: {
-                files: ['test/spec/{,*/}*.coffee'],
-                tasks: ['coffee:test']
-            },
-            compass: {
-                files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
-                tasks: ['compass:server']
-            },
+//            coffee: {
+//                files: ['<%= yeoman.app %>/scripts/{,*/}*.coffee'],
+//                tasks: ['coffee:dist']
+//            },
+//            coffeeTest: {
+//                files: ['test/spec/{,*/}*.coffee'],
+//                tasks: ['coffee:test']
+//            },
+//            compass: {
+//                files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
+//                tasks: ['compass:server']
+//            },
             livereload: {
                 files: [
                     '<%= yeoman.app %>/*.html',
@@ -120,43 +120,43 @@ module.exports = function (grunt) {
                 }
             }
         },
-        coffee: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= yeoman.app %>/scripts',
-                    src: '{,*/}*.coffee',
-                    dest: '.tmp/scripts',
-                    ext: '.js'
-                }]
-            },
-            test: {
-                files: [{
-                    expand: true,
-                    cwd: 'test/spec',
-                    src: '{,*/}*.coffee',
-                    dest: '.tmp/spec',
-                    ext: '.js'
-                }]
-            }
-        },
-        compass: {
-            options: {
-                sassDir: '<%= yeoman.app %>/styles',
-                cssDir: '.tmp/styles',
-                imagesDir: '<%= yeoman.app %>/images',
-                javascriptsDir: '<%= yeoman.app %>/scripts',
-                fontsDir: '<%= yeoman.app %>/styles/fonts',
-                importPath: 'app/components',
-                relativeAssets: true
-            },
-            dist: {},
-            server: {
-                options: {
-                    debugInfo: true
-                }
-            }
-        },
+//        coffee: {
+//            dist: {
+//                files: [{
+//                    expand: true,
+//                    cwd: '<%= yeoman.app %>/scripts',
+//                    src: '{,*/}*.coffee',
+//                    dest: '.tmp/scripts',
+//                    ext: '.js'
+//                }]
+//            },
+//            test: {
+//                files: [{
+//                    expand: true,
+//                    cwd: 'test/spec',
+//                    src: '{,*/}*.coffee',
+//                    dest: '.tmp/spec',
+//                    ext: '.js'
+//                }]
+//            }
+//        },
+//        compass: {
+//            options: {
+//                sassDir: '<%= yeoman.app %>/styles',
+//                cssDir: '.tmp/styles',
+//                imagesDir: '<%= yeoman.app %>/images',
+//                javascriptsDir: '<%= yeoman.app %>/scripts',
+//                fontsDir: '<%= yeoman.app %>/styles/fonts',
+//                importPath: 'app/components',
+//                relativeAssets: true
+//            },
+//            dist: {},
+//            server: {
+//                options: {
+//                    debugInfo: true
+//                }
+//            }
+//        },
         // not used since Uglify task does concat,
         // but still available if needed
         /*concat: {
@@ -263,16 +263,16 @@ module.exports = function (grunt) {
         },
         concurrent: {
             server: [
-                'coffee:dist',
-                'compass:server'
+//                'coffee:dist',
+//                'compass:server'
             ],
             test: [
-                'coffee',
-                'compass'
+//                'coffee',
+//                'compass'
             ],
             dist: [
-                'coffee',
-                'compass:dist',
+//                'coffee',
+//                'compass:dist',
                 'imagemin',
                 'svgmin',
                 'htmlmin'
