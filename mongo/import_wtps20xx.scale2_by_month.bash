c40c0bc2f9abf91081a8265cc3523b3bb21147ad
root <root@i-1603-29759-VM.cs643cloud.internal>
2013-05-14 02:17:41 +0900

    eval import scale2_by_month.

--- mongo/import_wtps20xx.scale2_by_month.bash
+++ mongo/import_wtps20xx.scale2_by_month.bash
@@ -2,9 +2,9 @@
 # 
 
 for i in `seq 2001 2012`; do
-	CMD="mongoimport -h 10.1.1.35 -d wtps20xx -c scale2_by_month < wtps$i.scale2_by_month.json"
+	CMD="mongoimport -h 10.1.1.35 -d wtps20xx -c scale2_by_month_ < wtps$i.scale2_by_month.json"
 	echo $CMD
-	$CMD
+	eval $CMD
 done
 
 
